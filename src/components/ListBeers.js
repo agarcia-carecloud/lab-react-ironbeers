7635ba8233426d7e9b15ed51f1af7538fe915a95
Alejandro Garcia <agarc371@gmail.com>
2020-10-10 16:17:32 -0400

    complete iteration 3

--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function ListBeers() {
-  return (
-    <div>
-      <header>
-        <Link to="/">
-          <img src="/images/home-icon.png" alt="home" />
-        </Link>
-      </header>
-      <h2>All Beers</h2>
-    </div>
-  );
+class ListBeers extends Component {
+  state = {
+    listOfBeers: null,
+  };
+
+  componentDidMount() {
+    Axios.get('https://ih-beers-api2.herokuapp.com/beers')
+      .then((responseFromApi) => {
+        console.log({ beers: responseFromApi.data });
+        this.setState({ listOfBeers: responseFromApi.data });
+      })
+      .catch((err) =>
+        console.log(`error retrieving the list of beers: ${err}`)
+      );
+  }
+
+  displayBeersList() {
+    return this.state.listOfBeers.map((beer, i) => {
+      return (
+        <div key={i} className="list-item-box centerContent">
+          <img className="beer-image" src={beer.image_url} alt="beer" />
+
+          <Link to={`/beers/${beer.id}`}>{beer.name}</Link>
+          <br />
+          <h3>{beer.tagline}</h3>
+          <h5>Created by: {beer.contributed_by}</h5>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to="/">
+            <img src="/images/home-icon.png" alt="home" />
+          </Link>
+        </header>
+        <h2>All Beers</h2>
+        <div className="centerContent list-item-container">
+          {this.state.listOfBeers && this.displayBeersList()}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default ListBeers;
