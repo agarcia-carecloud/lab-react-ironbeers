32328a1959f6d922243194f61aa20e48f59fcbf9
Alejandro Garcia <agarc371@gmail.com>
2020-10-10 14:51:01 -0400

    initial commit, prepare home page with links to other pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import ListBeers from './components/ListBeers';
+import BeerDetails from './components/BeerDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div className="centerContent homePage">
+      <div className="centerContent home-links">
+        <ul>
+          <img src="/images/beer-taps-1.jpg" alt="beer-taps-1" />
+          <li>
+            <Link to="/beers">All Beers</Link>
+          </li>
+          <br />
+          <img src="/images/beer-taps-2.jpg" alt="beer-taps-2" />
+          <li>
+            <Link to="/random-beer">Random Beer</Link>
+          </li>
+          <br />
+          <img src="/images/beer-taps-3.jpg" alt="beer-taps-3" />
+          <li>
+            <Link to="/new-beer">Add New Beer</Link>
+          </li>
+          <br />
+        </ul>
+      </div>
+    </div>
+  );
+}
